2eee44a2c2b7b95ab69cb5ebb1faf54bad7348fa
Jason Chang <jasonchang1712@gmail.com>
2025-06-30 10:00:29 -0500

    Refactor INSERT to push WHERE upstream.

--- src/sql/insert.sql
+++ src/sql/insert.sql
@@ -2,7 +2,7 @@ insert into
     edwprodhh.edi_837_parser.response
 select      sha2(METADATA$FILENAME)                                             as response_id,
             $1                                                                  as response_body,
-            row_number() over (partition by METADATA$FILENAME order by seq)     as response_body_index,
+            row_number() over (partition by METADATA$FILENAME order by seq)     as line_number,
             METADATA$FILENAME                                                   as file_name,
             current_date()                                                      as upload_date
 from        (
@@ -12,8 +12,8 @@ from        (
                 -- from        @edwprodhh.edi_837_parser.stg_response/xl212927fw_250426_i5.837
                 from        @edwprodhh.edi_837_parser.stg_response
                             (file_format => edwprodhh.edi_837_parser.format_txt)
+                where       METADATA$FILENAME not in (select file_name from edwprodhh.edi_837_parser.response_files)
             )
-where       METADATA$FILENAME not in (select file_name from edwprodhh.edi_837_parser.response_files)
 ;
 
 insert into
