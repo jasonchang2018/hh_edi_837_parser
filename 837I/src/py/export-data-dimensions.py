ce5a9f1562f3788b6c99b45b4da15f16bbdbe517
Jason Chang <jasonchang1712@gmail.com>
2025-10-24 11:46:32 -0500

    Restructure project directory.

--- 837I/src/py/export-data-dimensions.py
+++ 837I/src/py/export-data-dimensions.py
@@ -16,7 +16,7 @@ cur = conn.cursor()
 results_list_hb = cur.execute("SELECT * FROM edwprodhh.edi_837_parser.export_data_dimensions where hbpb in ('HB', 'BOTH')").fetchall()
 results_text_hb = "\n".join(t[0] for t in results_list_hb)
 
-target_file_name_hb = f"C:/Users/jchang/Desktop/Projects/edi-837-parser/data/out/export-hb-{today_str}.837"
+target_file_name_hb = f"C:/Users/jchang/Desktop/Projects/edi-837-parser/837I/data/out/export-837I-hb-{today_str}.837"
 
 with open(target_file_name_hb, "w", encoding="utf-8") as f:
     f.write(results_text_hb)
@@ -26,7 +26,7 @@ with open(target_file_name_hb, "w", encoding="utf-8") as f:
 results_list_pb = cur.execute("SELECT * FROM edwprodhh.edi_837_parser.export_data_dimensions where hbpb in ('PB', 'BOTH')").fetchall()
 results_text_pb = "\n".join(t[0] for t in results_list_pb)
 
-target_file_name_pb = f"C:/Users/jchang/Desktop/Projects/edi-837-parser/data/out/export-pb-{today_str}.837"
+target_file_name_pb = f"C:/Users/jchang/Desktop/Projects/edi-837-parser/837I/data/out/export-837I-pb-{today_str}.837"
 
 with open(target_file_name_pb, "w", encoding="utf-8") as f:
     f.write(results_text_pb)
