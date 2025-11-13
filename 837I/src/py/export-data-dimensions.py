1a497529009afd9b77576f4cc0dcfc3bb2ee2363
Jason Chang <jasonchang1712@gmail.com>
2025-11-13 12:49:27 -0600

    Clean comments.

--- 837I/src/py/export-data-dimensions.py
+++ 837I/src/py/export-data-dimensions.py
@@ -29,20 +29,13 @@ df = pd.DataFrame(results_list, columns = [desc[0] for desc in cur.description])
 ##  For each PL Group, Convert to Text and Export File
 for value, group_df in df.groupby("PL_GROUP"):
 
-    # Convert the relevant column(s) to your output text format.
-    # If you want entire rows, adjust this part.
-
     results_text = ("\n".join(
         group_df
             .sort_values(by = group_df.columns[1])
             .iloc[:, 0]
             .astype(str)
     ))
-
-    # results_text = "\n".join(group_df.iloc[:, 0].astype(str).tolist())  
-    # results_text_hb = "\n".join(t[0] for t in results_list)
-
-    # Build filename — include the distinct value
+    
     filename = (
         f"C:/Users/jchang/Desktop/Projects/edi-837-parser/837I/data/out/"
         f"export-837I-HB-{re.sub(r"[^\w]", "", value)}-{today_str}.837"
