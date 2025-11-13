3d16b1b34242950c0249670f63ea7dacae335a26
Jason Chang <jasonchang1712@gmail.com>
2025-11-13 12:33:24 -0600

    Add secrets file for Snowflake credentials. Update file creation to just HB and loop for each client.

--- 837I/src/py/export-data-dimensions.py
+++ 837I/src/py/export-data-dimensions.py
@@ -1,35 +1,58 @@
 import snowflake.connector
 from datetime import datetime
+from pathlib import Path
+import pandas as pd
+import re
 
 today_str = datetime.now().strftime("%Y%m%d")
 
+
+##  Read Snowflake Password
+with open("C:/Users/jchang/Desktop/Projects/edi-837-parser/837I/src/py/secrets.txt") as f:
+    password = f.read().strip()
+
 conn = snowflake.connector.connect(
-    user='****************',
-    password='****************',
-    account='****************',
-    warehouse='****************'
+    user='JCHANG',
+    password=password,
+    account='OITBPKZ-WPA42783',
+    warehouse='ANALYSIS_WH'
 )
 cur = conn.cursor()
 
 
-##  HB
-results_list_hb = cur.execute("SELECT * FROM edwprodhh.edi_837_parser.export_data_dimensions where hbpb in ('HB', 'BOTH')").fetchall()
-results_text_hb = "\n".join(t[0] for t in results_list_hb)
+##  Return Snowflake Results
+results_list = cur.execute("SELECT * FROM edwprodhh.edi_837i_parser.export_data_dimensions").fetchall()
 
-target_file_name_hb = f"C:/Users/jchang/Desktop/Projects/edi-837-parser/837I/data/out/export-837I-hb-{today_str}.837"
-
-with open(target_file_name_hb, "w", encoding="utf-8") as f:
-    f.write(results_text_hb)
+##  Convert to DF
+df = pd.DataFrame(results_list, columns = [desc[0] for desc in cur.description])
 
 
-##  PB
-results_list_pb = cur.execute("SELECT * FROM edwprodhh.edi_837_parser.export_data_dimensions where hbpb in ('PB', 'BOTH')").fetchall()
-results_text_pb = "\n".join(t[0] for t in results_list_pb)
+##  For each PL Group, Convert to Text and Export File
+for value, group_df in df.groupby("PL_GROUP"):
 
-target_file_name_pb = f"C:/Users/jchang/Desktop/Projects/edi-837-parser/837I/data/out/export-837I-pb-{today_str}.837"
+    # Convert the relevant column(s) to your output text format.
+    # If you want entire rows, adjust this part.
 
-with open(target_file_name_pb, "w", encoding="utf-8") as f:
-    f.write(results_text_pb)
+    results_text = ("\n".join(
+        group_df
+        .sort_values(by = group_df.columns[1])
+        .iloc[:, 0]
+        .astype(str)
+    ))
+
+    # results_text = "\n".join(group_df.iloc[:, 0].astype(str).tolist())  
+    # results_text_hb = "\n".join(t[0] for t in results_list)
+
+    # Build filename — include the distinct value
+    filename = (
+        f"C:/Users/jchang/Desktop/Projects/edi-837-parser/837I/data/out/"
+        f"export-837I-HB-{re.sub(r"[^\w]", "", value)}-{today_str}.837"
+    )
+
+    with open(filename, "w") as f:
+        f.write(results_text)
+
+    print(f"Created file: {filename}")
 
 
 cur.close()
