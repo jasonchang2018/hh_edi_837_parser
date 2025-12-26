df7915f35e0242eefdcc6cc2b32fbb195dc1e7d2
Jason Chang <jasonchang1712@gmail.com>
2025-12-26 07:42:36 -0600

    Remove legacy code.

--- 277/src/sql/hl_20_payer.sql
+++ 277/src/sql/hl_20_payer.sql
@@ -323,39 +323,6 @@ from        hl
 ;
 
 
-select      header_st.response_id,
-            header_st.nth_functional_group,
-            header_st.nth_transaction_set,
-            header_st.transaction_set_header,
-            header_st.transaction_set_id_code,
-            header_st.transaction_set_control_number_header,
-            header_st.implementation_convention_reference,
-            trailer_se.transaction_set_trailer,
-            trailer_se.transaction_segment_count,
-            trailer_se.transaction_set_control_number_trailer,
-            bht.bht_prefix,
-            bht.hierarchical_structure_code,
-            bht.transaction_set_purpose_code,
-            bht.originator_application_transaction_id,
-            bht.transaction_set_created_date,
-            bht.transaction_set_created_time,
-            bht.transaction_type_code,
-            bht.transaction_set_created_timestamp
-
-from        header_st
-            left join
-                trailer_se
-                on  header_st.response_id           = trailer_se.response_id
-                and header_st.nth_functional_group  = trailer_se.nth_functional_group
-                and header_st.nth_transaction_set   = trailer_se.nth_transaction_set
-            left join
-                bht
-                on  header_st.response_id           = bht.response_id
-                and header_st.nth_functional_group  = bht.nth_functional_group
-                and header_st.nth_transaction_set   = bht.nth_transaction_set
-;
-
-
 
 create or replace task
     edwprodhh.edi_277_parser.insert_hl_20_payer
@@ -404,6 +371,7 @@ with filtered as
     from        edwprodhh.edi_277_parser.response_flat
     where       hl_index is not null
                 and lag_hl_indicator = '20'
+                and response_id not in (select response_id from edwprodhh.edi_277_parser.hl_20_payer)
 )
 , hl as
 (
