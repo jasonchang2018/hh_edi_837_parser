155359072d7d618529229789381f702a0bd8a4c9
Jason Chang <jasonchang1712@gmail.com>
2025-12-29 07:17:45 -0600

    Add archive commands for 277 and 835 retrieval.

--- 277/src/sh/import-sftp.ps1
+++ 277/src/sh/import-sftp.ps1
@@ -1,3 +1,4 @@
+####  Logon to SFTP
 sftp -P 522 `
   -oHostKeyAlgorithms=+ssh-rsa `
   -oPubkeyAcceptedAlgorithms=+ssh-rsa `
@@ -5,6 +6,25 @@ sftp -P 522 `
 
 DBO6RS9pF18ouiW
 
+
+####  Export locally from SFTP
 sftp> cd 837P/OUT
 sftp> get *.277 "J:/DATA_DIMENSIONS/IN/"
 sftp> exit
+
+
+####  Archive within SFTP
+# 1) Create a file list based on what you downloaded
+cd "J:/DATA_DIMENSIONS/IN/"
+$localDir = "J:/DATA_DIMENSIONS/IN/"
+$files = Get-ChildItem $localDir -Filter "*.277" | Select-Object -ExpandProperty Name
+
+# 2) Generate sftp commands
+$cmds = @()
+foreach ($f in $files) {
+  $cmds += "rename $f archive-277/$f"
+}
+$cmds += "quit"
+
+# 3) Write the batch file. Currently, just copy and paste it manually into the SFTP terminal.
+$cmds | Set-Content -Encoding ASCII .\sftp-archive-277.txt
--- 835/src/sh/import-sftp.ps1
+++ 835/src/sh/import-sftp.ps1
@@ -8,3 +8,26 @@ DBO6RS9pF18ouiW
 sftp> cd 837P/OUT
 sftp> get *.835 "J:/DATA_DIMENSIONS/IN/"
 sftp> exit
+
+
+####  Export locally from SFTP
+sftp> cd 837P/OUT
+sftp> get *.835 "J:/DATA_DIMENSIONS/IN/"
+sftp> exit
+
+
+####  Archive within SFTP
+# 1) Create a file list based on what you downloaded
+cd "J:/DATA_DIMENSIONS/IN/"
+$localDir = "J:/DATA_DIMENSIONS/IN/"
+$files = Get-ChildItem $localDir -Filter "*.835" | Select-Object -ExpandProperty Name
+
+# 2) Generate sftp commands
+$cmds = @()
+foreach ($f in $files) {
+  $cmds += "rename $f archive-835/$f"
+}
+$cmds += "quit"
+
+# 3) Write the batch file. Currently, just copy and paste it manually into the SFTP terminal.
+$cmds | Set-Content -Encoding ASCII .\sftp-archive-835.txt
